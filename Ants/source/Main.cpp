ac25b0bd3a9795190c3743f7cb988d0a7f3d5474
Lucas Laukien <lucaslaukien@gmail.com>
2014-12-21 15:46:09 -0500

    Added JS

--- Ants/source/Main.cpp
+++ Ants/source/Main.cpp
@@ -1,91 +1,35 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+#include <js/Javascript.h>
+
+#include <memory>
+#include <iostream>
+#include <string>
+
+void js_print(const CFunctionsScopePtr &v, void* extraData) {
+	std::cout << v->getArgument("text")->getString().c_str() << std::endl;
+}
 
 int main() {
-	sf::RenderWindow window;
-	window.create(sf::VideoMode(1280, 800), "ALife");
+	std::shared_ptr<CTinyJS> js = std::make_shared<CTinyJS>();
 
-	window.setVerticalSyncEnabled(true);
+	/* add the functions from TinyJS_Functions.cpp */
+	registerFunctions(js.get());
+	registerStringFunctions(js.get());
+	registerMathFunctions(js.get());
 
-	sf::Clock clock;
+	/* Add a native function */
+	js->addNative("function print(text)", js_print, 0);
 
-	float dt = 0.017f;
-
-	sf::View view;
-	sf::View newView;
-
-	view.setSize(sf::Vector2f(static_cast<float>(window.getSize().x), static_cast<float>(window.getSize().y)));
-	view.setCenter(sf::Vector2f(worldObjectGrid->getWidth() * 0.5f * worldObjectGrid->_cellSize.x, worldObjectGrid->getHeight() * 0.5f * worldObjectGrid->_cellSize.y));
-
-	newView = view;
-
-	float zoomRate = 0.2f;
-
-	float viewInterpolateRate = 20.0f;
-
-	sf::Vector2i prevMousePos = sf::Mouse::getPosition();
-
-	bool quit = false;
+	js->executeSafe("var lets_quit = 0; function quit() { lets_quit = 1; }");
+	js->executeSafe("print(\"Interactive mode... Type quit(); to exit, or print(...); to print something.\");");
 	
-	do {
-		sf::Event windowEvent;
+	int lineNumber = 0;
+	while (js->evaluate("lets_quit") == "0") {
+		std::string buffer;
+		std::cout << ">";
+		std::getline(std::cin, buffer);
+		js->executeSafe(buffer, "stdin", lineNumber++);
+	}
 
-		while (window.pollEvent(windowEvent)) {
-			switch (windowEvent.type)
-			{
-			case sf::Event::Closed:
-				quit = true;
-
-				break;
-
-			case sf::Event::MouseWheelMoved:
-				int dWheel = windowEvent.mouseWheel.delta;
-
-				newView = view;
-
-				window.setView(newView);
-
-				sf::Vector2f mouseZoomDelta0 = window.mapPixelToCoords(sf::Mouse::getPosition(window));
-
-				newView.setSize(view.getSize() + newView.getSize() * (-zoomRate * dWheel));
-
-				window.setView(newView);
-
-				sf::Vector2f mouseZoomDelta1 = window.mapPixelToCoords(sf::Mouse::getPosition(window));
-
-				window.setView(view);
-
-				newView.setCenter(view.getCenter() + mouseZoomDelta0 - mouseZoomDelta1);
-
-				break;
-			}
-		}
-
-		view.setCenter(view.getCenter() + (newView.getCenter() - view.getCenter()) * viewInterpolateRate * dt);
-		view.setSize(view.getSize() + (newView.getSize() - view.getSize()) * viewInterpolateRate * dt);
-
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			sf::Vector2i dPos = sf::Mouse::getPosition() - prevMousePos;
-
-			view.setCenter(view.getCenter() - sf::Vector2f(dPos.x * (view.getSize().x / window.getSize().x), dPos.y * (view.getSize().x / window.getSize().x)));
-
-			newView.setCenter(view.getCenter());
-		}
-
-		prevMousePos = sf::Mouse::getPosition();
-
-		window.clear();
-
-		window.setView(view);
-
-		world.render(window);
-
-		window.display();
-
-		//dt = clock.getElapsedTime().asSeconds();
-		clock.restart();
-	} while (!quit);
-
+	system("pause");
 	return 0;
 }
---
+++ Ants/source/js/Javascript.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <js/TinyJS.h>
+#include <js/TinyJS_Functions.h>
+#include <js/TinyJS_StringFunctions.h>
+#include <js/TinyJS_MathFunctions.h>
